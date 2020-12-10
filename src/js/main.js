3dad2e221c53ee3d218f8036d97efc05b9bfa15c
ShawTim <shawtim.liu@gmail.com>
2020-12-11 02:16:16 +0800

    add small/medium width toggle support

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,24 @@ import FileSaver from "file-saver";
 document.addEventListener("DOMContentLoaded", (e) => {
   const container = document.querySelector(".container");
   const downloadBtn = document.getElementById("download-button");
+  const defaultBtn = document.querySelector(".options input[name=width][value=s]");
+  const toggleBtns = document.querySelectorAll(".options input[name=width]");
 
   downloadBtn.addEventListener("click", (e) => {
     html2canvas(container, { scrollY: -window.scrollY }).then((canvas) => {
       canvas.toBlob((blob) => FileSaver.saveAs(blob, "Facebook-Factcheck.png"));
     });
   });
+  [...toggleBtns].forEach((btn) => btn.addEventListener("click", (e) => {
+    if (e.target.value === "s") {
+      const classes = container.className.split(" ").filter((c) => !!c && c !== "small");
+      container.className = classes.concat("small").join(" ");
+    }
+    if (e.target.value === "m") {
+      const classes = container.className.split(" ").filter((c) => !!c && c !== "small");
+      container.className = classes.join(" ");
+    }
+  }));
+
+  defaultBtn.click();
 });
