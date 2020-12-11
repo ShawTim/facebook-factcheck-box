2ac12f90b733a4cf4be0403050af09d1980165e8
ShawTim <shawtim.liu@gmail.com>
2020-12-12 00:48:42 +0800

    add HKG icon as default

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,17 @@ import FileSaver from "file-saver";
 document.addEventListener("DOMContentLoaded", (e) => {
   const container = document.querySelector(".container");
   const downloadBtn = document.getElementById("download-button");
-  const defaultBtn = document.querySelector(".options input[name=width][value=s]");
-  const toggleBtns = document.querySelectorAll(".options input[name=width]");
+  const defaultIconBtn = document.querySelector(".options input[name=icon][value=hkg]");
+  const toggleIconBtns = document.querySelectorAll(".options input[name=icon]");
+  const defaultWidthBtn = document.querySelector(".options input[name=width][value=s]");
+  const toggleWidthBtns = document.querySelectorAll(".options input[name=width]");
 
   downloadBtn.addEventListener("click", (e) => {
     html2canvas(container, { scrollY: -window.scrollY }).then((canvas) => {
       canvas.toBlob((blob) => FileSaver.saveAs(blob, "Facebook-Factcheck.png"));
     });
   });
-  [...toggleBtns].forEach((btn) => btn.addEventListener("click", (e) => {
+  [...toggleWidthBtns].forEach((btn) => btn.addEventListener("click", (e) => {
     if (e.target.value === "s") {
       const classes = container.className.split(" ").filter((c) => !!c && c !== "small");
       container.className = classes.concat("small").join(" ");
@@ -22,6 +24,17 @@ document.addEventListener("DOMContentLoaded", (e) => {
       container.className = classes.join(" ");
     }
   }));
+  [...toggleIconBtns].forEach((btn) => btn.addEventListener("click", (e) => {
+    if (e.target.value === "hkg") {
+      const classes = container.className.split(" ").filter((c) => !!c && c !== "hkg-icon");
+      container.className = classes.concat("hkg-icon").join(" ");
+    }
+    if (e.target.value === "facebook") {
+      const classes = container.className.split(" ").filter((c) => !!c && c !== "hkg-icon");
+      container.className = classes.join(" ");
+    }
+  }));
 
-  defaultBtn.click();
+  defaultIconBtn.click();
+  defaultWidthBtn.click();
 });
